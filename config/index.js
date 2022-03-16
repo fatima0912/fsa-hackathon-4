80629192e5074e471bcc0eeab5f1f7fdb08bf47f
juveria fatima <fatima0912juveria@gmail.com>
2022-03-16 10:14:52 +0530

    port

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const config={
-dbConStr:'mongodb+srv://juveria2803:sofia2803@cluster0.exeqi.mongodb.net/ecom'
+dbConStr:'mongodb+srv://juveria2803:sofia2803@cluster0.exeqi.mongodb.net/ecommerce'
 }
 
 module.exports=config;
